f60db18e3e1e153a9c9cb054acb0809932a9cf98
Gema <gemrodper@gmail.com>
2017-11-23 10:49:06 +0100

    Adding the Research Track: call for papers

--- loadhead.js
+++ loadhead.js
@@ -6,6 +6,7 @@ if (level==1) {
 	var indexurl = "index.html";
 	var venueurl = "venue.html";
 	var orgcommitteeurl = "OrganizationCommittee.html";
+	var ResearchTrackCFP = "cfp/ResearchTrackCFP.html";
 /*
 	var Registrationurl = "registration.html";
 	var Programurl = "ToBeAnnounced.html";
@@ -41,6 +42,7 @@ else if (level>=2) {
 	var indexurl = "../index.html";
 	var venueurl = "../venue.html";
 	var orgcommitteeurl = "../OrganizationCommittee.html";
+	var ResearchTrackCFP = "../cfp/ResearchTrackCFP.html";
 
 /*
 	var Registrationurl = "../registration.html";
@@ -104,6 +106,7 @@ navpartcontainer.append(navbarheader);
 var navbarcollapse = $('<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"></div>');
 var navbarcollapseul = $('<ul class="nav navbar-nav navbar-right" id="id_navbarul"></ul>');
 
+
 /*
 
 var li_registration = $('<li><a href="'+Registrationurl+'">Registration</a></li>');
@@ -118,18 +121,22 @@ var li_program = $('<li class="dropdown"><a href="'+Programurl+'" class="dropdow
 
 navbarcollapseul.append(li_program);
       
-
+*/
 
 var li_cfp = $('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Call for Papers<span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></li>');        
-li_cfp.find('ul').append('<li><a href="'+ResearchTrackCFP+'">Research Track</a></li>');  
+
+li_cfp.find('ul').append('<li><a href="'+ResearchTrackCFP+'">Research Track</a></li>'); 
+/*
 li_cfp.find('ul').append('<li><a href="'+ERATrackCFP+'"">NIER Track</a></li>');   
 li_cfp.find('ul').append('<li><a href="'+IndustryTrackCPF+'">Industry Track</a></li>');   
 li_cfp.find('ul').append('<li><a href="'+ToolDemoTrackCFP+'">Tool Demo Track</a></li>');   
 li_cfp.find('ul').append('<li><a href="'+ArtifactsTrackCFP+'">Artifacts Track</a></li>');
 li_cfp.find('ul').append('<li><a href="'+DoctoralCFP+'">Doctoral Symposium</a></li>');
-li_cfp.find('ul').append('<li><a href="'+CallForJournalFirstPresentation+'">Journal First Presentation</a></li>');           
-navbarcollapseul.append(li_cfp);        
-*/          
+li_cfp.find('ul').append('<li><a href="'+CallForJournalFirstPresentation+'">Journal First Presentation</a></li>');   
+*/ 
+navbarcollapseul.append(li_cfp); 
+     
+         
 var li_coevents = $('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Co-Located Events<span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></li>');       
 li_coevents.find('ul').append('<li><a href="http://www.ieee-scam.org/2018/">SCAM 2018</a></li>');
 li_coevents.find('ul').append('<li><a href="http://vissoft18.etsii.urjc.es">VISSOFT 2018</a></li>');
